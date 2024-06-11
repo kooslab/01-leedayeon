b12b1636e3c94e117b0b3a7520babb85a44f80e0
ldyno0 <leedayeon0486@gmail.com>
2024-06-11 02:00:31 +0000

    가나다라

--- db.py
+++ db.py
@@ -1,50 +1,49 @@
 data = [
     {
         "id": 1,
-        "plant_nm": "깻잎",
-        "family_kor_nm": "깻잎과",
-        "family_nm": "Brassica",
-        "genus_kor_nm": "깻잎속",
+        "plant_nm": "음식",
+        "family_kor_nm": "면",
+        "family_nm": "우동",
+        "genus_kor_nm": "크림우동",
         "genus_nm": "Brassica",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr8Uh-buvRfMZZXw4LYjN5OZthg6mmEIFsWA&s",
-        "desc": "깻잎은 맛있다"
+        "img_url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA3MDZfMjcz%2FMDAxNjg4NjQ0NzA4NzI3.-0MuCWPUnlmfypYYD14-PExqC184ON1KnAZ3QpQSIX4g.VPpqjm8zeOLok8klBRriXSJJIzbVN2PVC5AQEVr6-CQg.JPEG.phyt82%2FKakaoTalk_20230706_205444392.jpg&type=sc960_832",
+        "desc": "크림우동은 맛있다"
     },
     {
         "id": 2,
-        "plant_nm": "코코아잎",
-        "family_kor_nm": "코코아잎과",
-        "family_nm": "Coccaceae",
-        "genus_kor_nm": "코코아속",
-        "genus_nm": "Coccus",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2GJiBJWcs7UlQQDc5ceHK1li4x22AboerzA&s",
-        "desc": "코코아 하하"
+        "plant_nm": "사과나무",
+        "family_kor_nm": "장미과",
+        "family_nm": "Malus Pumila",
+        "genus_kor_nm": "사과나무속",
+        "genus_nm": "Malus Pumila",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Apple_Tree_Flower_Blossoms_Macro_PLT-FL-AB-6.jpg/1280px-Apple_Tree_Flower_Blossoms_Macro_PLT-FL-AB-6.jpg",
+        "desc": "사과나무 ~ "
         },
     {
         "id": 3,
-        "plant_nm": "소나무",
-        "family_kor_nm": "소나무과",
-        "family_nm": "Solanaceae",
-        "genus_kor_nm": "소나무속",
-        "genus_nm": "Solanum",
-        "img_url": "https://i.namu.wiki/i/HPXSQ7eVZsn3oSvNtBItitmeqVnuGq6W-yNzxjsiNAJBgwWM4lr-aIUvr1XEFMVdkEkaOfL4DsesDgaOaZVU-A.webp",
-        "desc": "소나무는 맛있다"
+        "plant_nm": "체리",
+        "family_kor_nm": "장미과",
+        "family_nm": "Prunus avium",
+        "genus_kor_nm": "벚나무속",
+        "genus_nm": "Prunus avium",
+        "img_url": "https://i.namu.wiki/i/ykqf8b-Ff8nQyjRysVPco_DEM5nDhJ8SdxlvsczSiGXKfpjAtXKdQ7CagF_OJrAJ5X4r-5aCHEGuAjEppgo_16WBdgxIZFK3frdcjo2TYufA-ardoBi79XJPdnDUFt6NrrhpyI6TtHE6rCm1-EFCMQ.webp"
         },
     {
         "id": 4,
-        "plant_nm": "무궁화",
-        "family_kor_nm": "무궁화과",
-        "family_nm": "Monocotyledon",
-        "genus_kor_nm": "무궁화속",
-        "genus_nm": "Monocotyledon",
-        "img_url": "https://i.namu.wiki/i/U3xs4ovgGfDKlDujDDa4amcUg9F4xjGKE5X3ZeNc6Jmg1NOxcy6HIJPLCDsNIHDlbgYAcrEui2-Z_H6VsJ921A.webp",
-        "desc": "무궁화는 맛있다"    },
+        "plant_nm": "무화과",
+        "family_kor_nm": "뽕나무과",
+        "family_nm": "fig",
+        "genus_kor_nm": "무화과",
+        "genus_nm": "fig",
+        "img_url": "https://dbscthumb-phinf.pstatic.net/2765_000_374/20210408061540455_L77C0ZZJW.gif/65296_0.gif?type=m1500&wm=N",
+        "desc": "무화과는 맛있다"    },
     {
         "id": 5,
-        "plant_nm": "벚꽅",
-        "family_kor_nm": "벚꽃과",
-        "family_nm": "Monocotyledon",
-        "genus_kor_nm": "벚꽃속",
-        "genus_nm": "Monocotyledon",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCszt6vqLKVVlTYgd4QBy505uM-5gLsoCLA&s",
-        "desc": "벚꽃 놀이가자"    }
+        "plant_nm": "자두",
+        "family_kor_nm": "장미과",
+        "family_nm": "Prunus salicina",
+        "genus_kor_nm": "벚나무속",
+        "genus_nm": "Prunus salicina",
+        "img_url": "https://dbscthumb-phinf.pstatic.net/3041_000_1/20140521150655715_2OY2ZNHM0.jpg/WLI95_4_650.jpg?type=m4500_4500_fst_n&wm=Y",
+        "desc": "자두자두 졸려"    }
 ]
